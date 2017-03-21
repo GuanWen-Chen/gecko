92a030d490227a92669790426f61da6165a7c5ef
Kevin Chen <kechen@mozilla.com>
2017-03-21 03:57:56 +0000

    Bug 1343754 - Label PTexture. r?bevistseng, nical
    MozReview-Commit-ID: 1PnDEQvz9FK

--- gfx/layers/ipc/CompositorBridgeChild.h
+++ gfx/layers/ipc/CompositorBridgeChild.h
@@ -130,7 +130,8 @@ public:
   virtual PTextureChild* CreateTexture(const SurfaceDescriptor& aSharedData,
                                        LayersBackend aLayersBackend,
                                        TextureFlags aFlags,
-                                       uint64_t aSerial) override;
+                                       uint64_t aSerial,
+                                       uint64_t aLayersId) override;
 
   virtual void HandleFatalError(const char* aName, const char* aMsg) const override;
 
